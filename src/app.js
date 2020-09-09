dd3e07e66c48ed3a37ac9e2ad60e4f4c715e6730
Mostafa Salama <mostafatourad789@gmail.com>
2020-09-09 20:09:37 +0200

    create the simple app with views with pug

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const path = require('path') ;
 const express = require('express') ;
+const dotEnv = require('dotenv');
 dotEnv.config() ;
 
 const app = express() ;
 app.use(express.json()) ;
 app.use(express.urlencoded({extended:false})) ;
-app.use(express.static(path.join(__dirname,'public')))
+app.use('/public',express.static(path.join(__dirname,'public')))
 app.set('view engine','pug') ;
 app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res)=>{
--- src/bin/index.js
+++ src/bin/index.js
@@ -1,1 +1,11 @@
-console.log('app is running');
+const http = require('http');
+const chalk = require('chalk');
+const app = require('../app');
+const port = process.env.PORT || 8000;
+
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, () => {
+	console.log(chalk.yellow(`app is running at ${port}`));
+});
