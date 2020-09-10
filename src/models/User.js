4934d70ed39a9ef7c10c6bc18c2342321b6dd859
Mostafa Salama <mostafatourad789@gmail.com>
2020-09-10 11:30:50 +0200

    add user model with todo schema

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose') ;
+
+const todoSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    text:{
+        type:String,
+        required:[true,'every todo should have a text']
+    }
+},{
+    timestamp:true
+})
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    todoList:[todoSchema]
+})
+
+const User = mongoose.model('User',userSchema);
+module.exports = User ; 
